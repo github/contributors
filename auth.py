456ed2ba01d470131c3e87fccae0e9e5756d8666
Zack Koppert <zkoppert@github.com>
2023-10-05 15:28:48 -0700

    Simplify auth system
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- auth.py
+++ auth.py
@@ -1,17 +1,12 @@
 """This is the module that contains functions related to authenticating to GitHub with a personal access token."""
 
-import os
 import github3
 
 
-def auth_to_github():
+def auth_to_github(token: str) -> github3.GitHub:
     """Connect to GitHub.com or GitHub Enterprise, depending on env variables."""
-    ghe = os.getenv("GH_ENTERPRISE_URL", default="").strip()
-    token = os.getenv("GH_TOKEN")
-    if ghe and token:
-        github_connection = github3.github.GitHubEnterprise(ghe, token=token)
-    elif token:
-        github_connection = github3.login(token=os.getenv("GH_TOKEN"))
+    if token:
+        github_connection = github3.login(token=token)
     else:
         raise ValueError("GH_TOKEN environment variable not set")
 
--- test_auth.py
+++ test_auth.py
@@ -9,42 +9,24 @@ class TestAuth(unittest.TestCase):
     Test case for the auth module.
     """
 
-    @patch("os.getenv")
-    @patch("github3.github.GitHubEnterprise")
-    def test_auth_to_github_enterprise(self, mock_github_enterprise, mock_getenv):
-        """
-        Test the auth_to_github function when the GH_ENTERPRISE_URL and GH_TOKEN environment variables are set.
-        """
-        mock_getenv.side_effect = ["https://github.enterprise.com", "token"]
-        mock_github_enterprise.return_value = "Authenticated to GitHub Enterprise"
-
-        result = auth.auth_to_github()
-
-        self.assertEqual(result, "Authenticated to GitHub Enterprise")
-
     @patch("github3.login")
-    @patch("os.getenv")
-    def test_auth_to_github_com(self, mock_login, mock_getenv):
+    def test_auth_to_github_with_token(self, mock_login):
         """
-        Test the auth_to_github function when only the GH_TOKEN environment variable is set.
+        Test the auth_to_github function when the token is provided.
         """
-        mock_getenv.side_effect = ["", "token"]
         mock_login.return_value = "Authenticated to GitHub.com"
 
-        result = auth.auth_to_github()
+        result = auth.auth_to_github("token")
 
-        self.assertEqual(result.url, "Authenticated to GitHub.com")
+        self.assertEqual(result, "Authenticated to GitHub.com")
 
-    @patch("os.getenv")
-    def test_auth_to_github_no_token(self, mock_getenv):
+    def test_auth_to_github_without_token(self):
         """
-        Test the auth_to_github function when the GH_TOKEN environment variable is not set.
+        Test the auth_to_github function when the token is not provided.
         Expect a ValueError to be raised.
         """
-        mock_getenv.side_effect = ["", ""]
-
         with self.assertRaises(ValueError):
-            auth.auth_to_github()
+            auth.auth_to_github("")
 
 
 if __name__ == "__main__":
